ad820ed10b3392e002f539324815b5a0c40607d7
Jury Razumau <mail@razumau.net>
2025-03-29 14:38:04 +0100

    run truedl job as many per-tournament jobs
    A single job is difficult to retry and requires a lot of memory. Each actual job instead will only calculate truedl for a single tournament.

--- app/jobs/truedl_for_all_tournaments_job.rb
+++ app/jobs/truedl_for_all_tournaments_job.rb
@@ -4,6 +4,17 @@ class TrueDLForAllTournamentsJob < ApplicationJob
   queue_as :default
 
   def perform(model_name)
-    TrueDLCalculator.calculate_for_all_tournaments_since_2021(model_name:)
+    model = Model.find_by(name: model_name)
+    unless model
+      Rails.logger.error "no model with the name #{model_name}"
+      return
+    end
+
+    tournaments = Tournament.where("start_datetime >= '2021-09-01'").pluck(:id)
+    single_tournament_jobs = tournaments.map do |tournament_id|
+      TrueDLForTournamentJob.new(model_name, tournament_id)
+    end
+
+    ActiveJob.perform_all_later(single_tournament_jobs)
   end
 end
