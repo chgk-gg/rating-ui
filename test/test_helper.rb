0952cb3a9f5eed60c4d3fc2555ca0c9e86610af0
Jury Razumau <mail@razumau.net>
2025-03-29 20:05:41 +0100

    set up vcr

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,6 +22,13 @@ end
 
 Capybara.default_driver = :rack_test
 
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :webmock
+  config.ignore_localhost = true
+  config.allow_http_connections_when_no_cassette = false
+end
+
 ModelIndexer.run
 MaterializedViews.recreate_all(model: InModel::DEFAULT_MODEL)
 
