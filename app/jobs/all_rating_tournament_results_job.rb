a47feae57450999dc2c6c51e2616ae2ab6b52513
Jury Razumau <mail@razumau.net>
2025-04-08 21:52:43 +0200

    move all tournament results jobs from rating-importer

---
+++ app/jobs/all_rating_tournament_results_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class AllRatingTournamentResultsJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    single_tournament_jobs = tournaments
+      .map { |tournament_id| SingleTournamentResultsJob.new(tournament_id) }
+
+    ActiveJob.perform_all_later(single_tournament_jobs)
+  end
+
+  def tournaments
+    Tournament.where(end_datetime: Date.new(2021, 9, 9)..Time.zone.today).pluck(:id)
+  end
+end
