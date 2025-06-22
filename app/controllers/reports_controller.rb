483bdc9a6f71929247aea22daff2fc1201db370f
Jury Razumau <mail@razumau.net>
2025-06-22 20:00:14 +0200

    remove all-time reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,8 +5,6 @@ class ReportsController < ActionController::Base
 
   def mau
     @active_rating_data = active_rating_players.map { |month| {x: month.month, y: month.players_count} }.to_json
-    @all_time_rating_data = old_rating_players.map { |month| {x: month.month, y: month.players_count} }.to_json
-    @all_time_data = all_players.map { |month| {x: month.month, y: month.players_count} }.to_json
     render layout: "nonmodel"
   end
 end
