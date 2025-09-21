547209ec2fe7cd4b97cf6ebda6b2db6a61eb9276
Jury Razumau <mail@razumau.net>
2025-09-21 18:36:32 +0200

    use continuations in PlayersJob

--- app/jobs/players_job.rb
+++ app/jobs/players_job.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+require "active_job/continuable"
+
 class PlayersJob < ApplicationJob
+  include ActiveJob::Continuable
+
   queue_as :chgk_info_import
   limits_concurrency to: 1, key: :chgk_info_api
 
@@ -9,31 +13,28 @@ class PlayersJob < ApplicationJob
   def perform
     @api_client = ChgkInfo::APIClient.new
 
-    page_number = 1
-    players = fetch_page(page_number)
+    step :fetch_page, start: 1 do |step|
+      loop do
+        players = @api_client.players(page: step.cursor)
+        break if players.empty?
 
-    while players.size > 0
-      player_rows = players.map do
-        {
-          id: it["id"],
-          first_name: it["name"],
-          patronymic: it["patronymic"],
-          last_name: it["surname"],
-          date_died: it["dateDied"],
-          got_questions_tag: it["gotQuestionsTag"]
-        }
+        player_rows = players.map do
+          {
+            id: it["id"],
+            first_name: it["name"],
+            patronymic: it["patronymic"],
+            last_name: it["surname"],
+            date_died: it["dateDied"],
+            got_questions_tag: it["gotQuestionsTag"]
+          }
+        end
+
+        ActiveRecord::Base.transaction do
+          Player.upsert_all(player_rows)
+        end
+
+        step.set!(step.cursor + 1)
       end
-
-      ActiveRecord::Base.transaction do
-        Player.upsert_all(player_rows)
-      end
-
-      page_number += 1
-      players = fetch_page(page_number)
     end
-  end
-
-  def fetch_page(page)
-    @api_client.players(page:)
   end
 end
