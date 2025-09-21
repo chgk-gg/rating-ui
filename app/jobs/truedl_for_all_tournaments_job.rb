3dfe81bff654d75b365d9fb6d614abbebf6bce5e
Jury Razumau <mail@razumau.net>
2025-09-21 16:42:38 +0200

    use continuations in TrueDLForAllTournamentsJob

--- app/jobs/truedl_for_all_tournaments_job.rb
+++ app/jobs/truedl_for_all_tournaments_job.rb
@@ -1,20 +1,27 @@
+# frozen_string_literal: true
+
+require "active_job/continuable"
 require_relative "../lib/truedl_calculator"
 
 class TrueDLForAllTournamentsJob < ApplicationJob
+  include ActiveJob::Continuable
+
   queue_as :wrappers
 
   def perform(model_name)
     model = Model.find_by(name: model_name)
     unless model
-      Rails.logger.error "no model with the name #{model_name}"
+      Rails.logger.error "No model with the name #{model_name}"
       return
     end
 
-    tournaments = Tournament.where("start_datetime >= '2021-09-01'").pluck(:id)
-    single_tournament_jobs = tournaments.map do |tournament_id|
-      TrueDLForTournamentJob.new(model_name, tournament_id)
-    end
+    tournaments = Tournament.where("start_datetime >= '2021-09-01'")
 
-    ActiveJob.perform_all_later(single_tournament_jobs)
+    step :calculate do |step|
+      tournaments.find_each(start: step.cursor, batch_size: 10) do |tournament|
+        TrueDLCalculator.calculate_for_tournament(tournament_id: tournament.id, model_name:)
+        step.advance! from: tournament.id
+      end
+    end
   end
 end
