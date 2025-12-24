1d3d979084ae29ccb1498e4878f339c372b46695
Jury Razumau <mail@razumau.net>
2025-12-24 16:44:19 +0100

    load pre-2021 tournaments on demand
    Older tournaments for players and teams are less useful, so we can load them only when a user want to see them.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,13 +6,22 @@ class PlayersController < ApplicationController
   def show
     player_id = params[:player_id].to_i
     @player = Player.find(player_id)
+    @model_name = current_model.name
 
     releases = current_model.player_releases(player_id:)
     @releases_detailed = current_model.player_rating_components(player_id:)
     tournaments = current_model.player_tournaments(player_id:)
     @rows = ReleaseTournamentBuilder.build(releases, tournaments, {})
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
 
+  def old_tournaments
+    player_id = params[:player_id].to_i
+    @player = Player.find(player_id)
+    @model_name = current_model.name
     @old_tournaments = current_model.player_old_tournaments(player_id:)
+    render partial: "old_tournaments_frame", layout: false
   rescue ActiveRecord::RecordNotFound
     render_404
   end
