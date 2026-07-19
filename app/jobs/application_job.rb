7aa3a6c85af99e79a2f65cde8a94819644b5b565
Jury Razumau <mail@razumau.net>
2026-07-19 12:19:35 +0200

    allow jobs to make longer database queries

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,11 +3,23 @@ class ApplicationJob < ActiveJob::Base
   MEDIUM_PRIORITY = 1
   LOW_PRIORITY = 2
 
+  STATEMENT_TIMEOUT = ENV.fetch("JOB_STATEMENT_TIMEOUT", 30_000).to_i
+
   queue_with_priority MEDIUM_PRIORITY
 
   # Ensure jobs always use the primary database for writes
   around_perform do |_job, block|
     ActiveRecord::Base.connected_to(role: :writing, &block)
+  end
+
+  # Allow jobs to run longer queries than the 5s limit for web requests.
+  around_perform do |_job, block|
+    ActiveRecord::Base.connection.execute("SET statement_timeout TO #{STATEMENT_TIMEOUT}")
+    block.call
+  ensure
+    web_timeout = ActiveRecord::Base.connection_db_config
+      .configuration_hash.dig(:variables, :statement_timeout) || 10_000
+    ActiveRecord::Base.connection.execute("SET statement_timeout TO #{web_timeout.to_i}")
   end
 
   # Ensure job scheduling (perform_later) uses primary database for Solid Queue
